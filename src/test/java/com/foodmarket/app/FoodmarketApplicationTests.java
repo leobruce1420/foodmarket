2a489fb460f4518774defe4eb5cd22215f363aff
Liao Li Hsuan <molamolaking860201@gmail.com>
2022-06-15 17:12:11 +0800

    初版

---
+++ src/test/java/com/foodmarket/app/FoodmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodmarket.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
