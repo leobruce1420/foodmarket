6d680753dacbfdde258856985d803cd3b44c0855
DESKTOP-NROIDJD\Student <x01masretep@gmail.com>
2022-06-21 09:43:15 +0800

    Merge commit '50fedb74c033117d97963b2cd0f4bcd54fc8d8fd' into product
    # Conflicts:
    #	src/main/webapp/WEB-INF/view/layout/navbar.jsp

--- src/main/java/com/foodmarket/app/product/controller/WorkProductController.java
+++ src/main/java/com/foodmarket/app/product/controller/WorkProductController.java
@@ -1,8 +1,13 @@
 package com.foodmarket.app.product.controller;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.util.codec.binary.Base64;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,37 +35,131 @@ public class WorkProductController {
 
 	@Autowired
 	private WorkProductService pmsgService;
-
-	@PostMapping("postProduct")
-	public String addMessage(@ModelAttribute("workProduct") ProductDto productDto, Model model) throws IOException {
-		WorkProduct newpMsg = new WorkProduct();
-//		System.out.println("qweqeqweqweweqw");
-//		String base64Str=productDto.getProductimgdto();
-//		if(!StringUtil.isBlank(path)) {
-//			
-//		}
-//		System.out.println(productDto.getProductimg());
-//		Util method = new Util();
-//		byte[] imgBytes = productDto.getProductimgdto(method.encoder(imgBytes));
-//		productDto.setProductimgdto(method.encoder(imgBytes));
+	// 圖片未成功
+//	@PostMapping("postProduct")
+//	public String addMessage(@RequestParam("imgFile") MultipartFile mf, Model model) throws IOException {
+////		System.out.println("qweqeqweqweweqw");
+////		String base64Str=productDto.getProductimgdto();
+////		if(!StringUtil.isBlank(path)) {
+////			
+////		}
+////		System.out.println(productDto.getProductimg());
+//		ProductDto method = new ProductDto();
+//		WorkProduct test = new WorkProduct();
+//		String producttype = productimgdto.getProductimgdto;
+//		byte[] imgBytes = mf.getBytes();
+//		System.out.println(imgBytes);
+//		String img = method.encoder(imgBytes);
 //		System.out.println(method.encoder(imgBytes));
-//		pmsgService.insertProduct(imgBytes);
+//		test.setProductimg(img);
+////		WorkProduct product = pmsgService.insertProduct(imgBytes);
 //		WorkProduct newpMsg = new WorkProduct();
-		WorkProduct lastestpMsg = pmsgService.getLastest();
+//		WorkProduct lastestpMsg = pmsgService.getLastest();
+//
+//		model.addAttribute("workProduct", newpMsg);
+//		model.addAttribute("lastestpMsg", lastestpMsg);
+//		return "product/addMessage";
+//	}
+	// img byte[]
+//	@PostMapping("postProduct")
+//	public String addNewFirm(@RequestParam String productname, @RequestParam String productcategory,
+//							 @RequestParam Integer productprice, @RequestPart MultipartFile productimg,
+//							 @RequestParam String imgtype,
+//							 @RequestParam String productdesciption,
+//							 @RequestParam Integer inventoryquantity,
+//							 @RequestParam String takedown,Model m) {
+//		WorkProduct newProduct = new WorkProduct();
+//
+//		String productType = productimg.getContentType();
+//
+//		System.out.println(productType);
+//
+//		if(!productType.startsWith("image")) {
+//			
+//			Map<String, String> errors = new HashMap<String, String>();
+//			errors.put("firmLogo", "檔案類型必須為圖片");
+//			
+//			ProductDto productDto = new ProductDto();
+//						
+//			m.addAttribute("errors", errors);
+//			m.addAttribute("productDto", productDto);
+//			return "product/addMessage";
+//		}
+//
+//		newProduct.setProductname(productname);
+//		newProduct.setProductcategory(productcategory);
+//		newProduct.setProductprice(productprice);
+//		newProduct.setImgtype(imgtype);
+//		newProduct.setProductdesciption(productdesciption);
+//		newProduct.setInventoryquantity(inventoryquantity);
+//		newProduct.setTakedown(takedown);
+//		try {
+//			newProduct.setProductimg(productimg.getBytes());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			ProductDto proDto = new ProductDto();
+//			m.addAttribute("productDto", proDto);
+//			return "product/addMessage";
+//		}
+//
+//		pmsgService.insertProduct(newProduct);
+//
+//		return "redirect:/product/all";
+//	}
 
-		model.addAttribute("workProduct", newpMsg);
-		model.addAttribute("lastestpMsg", lastestpMsg);
+	// base64
+	@PostMapping("postProduct")
+	public String addMessage(HttpServletRequest request,@RequestParam String productname, @RequestParam String productcategory,
+			 @RequestParam Integer productprice, @RequestParam("productimg") MultipartFile mf,
+			 @RequestParam String imgtype,
+			 @RequestParam String productdesciption,
+			 @RequestParam Integer inventoryquantity,
+			 @RequestParam String takedown,Model m) throws  IOException {
+		Util method = new Util();
+		try {
+//			if (!pmsgService.checkLoginSession(session)) {
+//				return "AdminLogin";
+//			}
+
+			request.setCharacterEncoding("UTF-8");
+			WorkProduct productbean = new WorkProduct();
+
+			productbean.setProductname(productname);
+			productbean.setProductcategory(productcategory);
+			productbean.setProductprice(productprice);
+			productbean.setImgtype(imgtype);
+			productbean.setProductdesciption(productdesciption);
+			productbean.setInventoryquantity(inventoryquantity);
+			productbean.setTakedown(takedown);
+			productbean.setImgtype("png");
+			productbean.setProductdesciption(productdesciption);
+			productbean.setInventoryquantity(inventoryquantity);
+
+			byte[] imgBytes = mf.getBytes();
+			productbean.setProductimg(method.encoder(imgBytes));
+
+
+			pmsgService.insertProduct(productbean);
+
+			WorkProduct Products = pmsgService.insertProduct();
+			m.addAttribute("products", Products);
+			return "product/addMessage";
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return "product/addMessage";
 	}
 
-	@GetMapping("product/editProduct")
-	public String editMessage(@RequestParam("productid") Long productid, Model model) {
-
-		WorkProduct pMsg = pmsgService.findById(productid);
-
-		model.addAttribute("pMsg", pMsg);
-		return "product/editMessage";
-	}
+//	@GetMapping("product/editProduct")
+//	public String editMessage(@RequestParam("productid") Long productid, Model model) {
+//		Optional<WorkProduct> opmsg = pmsgService.findById(productid);
+////		WorkProduct pMsg = pmsgService.findById(productid);
+//
+//		model.addAttribute("pMsg", opmsg);
+//		return "product/editMessage";
+//	}
 
 	@PostMapping("product/editProduct")
 	public String postMessage(@ModelAttribute(name = "pMsg") WorkProduct pMsg) {
@@ -88,7 +188,7 @@ public class WorkProductController {
 
 		return content;
 	}
-	//關鍵字查詢
+	// 關鍵字查詢
 //	@PostMapping("product/queryByName")
 //	public String queryNameAll(@RequestParam("productname") String productname, Model m, HttpSession session) {
 //
@@ -97,7 +197,7 @@ public class WorkProductController {
 //		m.addAttribute("productname", productname);
 //		return "product/viewoneMessages";
 //	}
-	
+
 //	商品種類查詢
 //	@PostMapping("product/category?productcategory=")
 //	public String queryNameAll(@RequestParam("productname") String productname, Model m, HttpSession session) {
